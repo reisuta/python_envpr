f5793330f5afc14778a7c348cccfdaef6105e3aa
amaki <in345vain@gmail.com>
2021-07-13 07:27:34 +0900

    python initial

---
+++ app.py
@@ -0,0 +1,22 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello world</h1>'
+
+# if __name__ == '__main__':
+#     app.run()
+
+print('hello wordl')
+print(2434)
+
+name = input('あなたの名前はなんですか？:')
+print(name)
+
+print ('あなたの名前は{}です'.format(name))
+
+
+a = b = 'hello'
+print(a,b)
