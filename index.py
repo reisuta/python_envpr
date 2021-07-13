4beab5497dcb0aa769362c93a4dd567e30c0406e
amaki <in345vain@gmail.com>
2021-07-14 07:57:09 +0900

    second commit

--- index.py
+++ index.py
@@ -8,3 +8,9 @@ print(fruit[-1])
 
 
 print(fruit.count('a'))
+
+num = 2
+sum = 4
+num_dict = {num: sum}
+
+print(dir(num_dict))
---
+++ pr.py
@@ -0,0 +1,61 @@
+# #1
+# num = 10
+
+# #2
+# print(type(num))
+
+# #3
+# num1 = type(num)
+# num_str = int(num)
+
+# #4
+# num_list =[num_str,'20','30']
+
+# #5 
+# num_list.append('40')
+
+# #6
+
+# num_tuple = tuple(num_list)
+
+# #7
+
+# #8
+# num_set = {}
+# #9
+# ans = num_tuple | num_set
+
+# #10
+# num_dict = {num_tuple:num_str}
+
+# #11
+# ans2 = len(num_list)
+
+# #12
+
+# #13
+# num_list.extend(['50','60']) 
+
+# #14
+# b = input()
+# is_under_50 =False
+# if b < 50:
+#     is_under_50 = True
+
+# #15
+
+# #16
+# print(dir(num_dict))
+
+
+# list = ['39', '32', '69']
+# for i, m in enumerate(list):
+#     print(i,m)
+    # print(m)
+
+# jaglj = ['aljg','ahgk','jaglj']
+# listB = [23,52,352,562,25]
+
+
+# for i,m in zip(jaglj, listB):
+#     print(i,m)
