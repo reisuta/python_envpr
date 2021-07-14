c2804dcf6cadfc104a85292edb4aef03514df6a7
amaki <in345vain@gmail.com>
2021-07-15 07:50:24 +0900

    map

--- pr.py
+++ pr.py
@@ -59,3 +59,60 @@
 
 # for i,m in zip(jaglj, listB):
 #     print(i,m)
+
+# def print_hello(a,b):
+#     if a<b:
+#         print('ajglaj')
+#     else:
+#         print('jlgaj')
+
+
+# print_hello(23,53)
+
+# def janken():
+#     a = ['goo', 'tyoki', 'paa']
+#     b = input()
+#     print('あなたの出した手:{}、相手の出した手:{}'.format(b,a[0]))
+
+# janken()
+
+# y= 23
+# x=8462 if y<0 else 382
+# print(x)
+
+# lamda_a = lambda s, e, w: s + e + w
+# print(lamda_a(1,6,4))
+
+# listA = ['2','23',5,35,3,2,1]
+
+# list_new = [int(x) for x in listA]
+
+# print(list_new)
+
+# man = {
+#     'name': 'ichiro',
+#     'age':'21',
+#     'country':'japan'
+# }
+# print(man['name'])
+
+# man = [1,3,2,2,3,45,5,2]
+
+# map_man =  map(lambda y:y + ' ' + man[y], man)
+# for i in map_man:
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
