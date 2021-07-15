810356e53797bfce77d1181bae52064315cc93e8
amaki <in345vain@gmail.com>
2021-07-16 08:01:22 +0900

    flask commit

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-# from flask import Flask
+from flask import Flask, render_template
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return '<h1>Hello world</h1>'
-
-# if __name__ == '__main__':
-#     app.run()
-
-print('hello wordl')
-print(2434)
-
-name = input('あなたの名前はなんですか？:')
-print(name)
-
-print ('あなたの名前は{}です'.format(name))
-
-
-a = b = 'hello'
-print(a,b)
-
-print('enf')
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.run()
