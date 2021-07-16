94d5cf7ba11ee40d2d43fdfa8d0a51f7f1fee735
amaki <in345vain@gmail.com>
2021-07-16 12:19:08 +0900

    add base.html

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import DEBUG
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,5 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
     
+@app.route('/home/<string:user_name>/<int:age>')
+def home(user_name, age):
+    login_user = {
+        'name':user_name,
+        'age':age
+    }
+    return render_template('home.html',user_info=login_user)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
