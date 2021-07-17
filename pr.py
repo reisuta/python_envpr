f65b5c39870fa7372966ef120a8afa8f4b6b22d2
amaki <in345vain@gmail.com>
2021-07-17 14:07:49 +0900

    pr add

--- pr.py
+++ pr.py
@@ -1,118 +1,25 @@
-# #1
-# num = 10
+list = {1:'cat',2:'dog',3:'elephant',4:'bird'}
 
-# #2
-# print(type(num))
+# print(list[4])
+# print(list.get(7,'does'))
+pri= list.keys()
+# sj = pri.split()
+for i in pri:
+    print(i)
+    
+# print(list.items())
 
-# #3
-# num1 = type(num)
-# num_str = int(num)
+# for k, v in list.items():
+#     print(k)
+#     print(v)
 
-# #4
-# num_list =[num_str,'20','30']
+# if k in list:
+#     print(list[k])
 
-# #5 
-# num_list.append('40')
+# list.update({5:'mmmm',6:'aaaa'})
 
-# #6
+# print(list)
 
-# num_tuple = tuple(num_list)
-
-# #7
-
-# #8
-# num_set = {}
-# #9
-# ans = num_tuple | num_set
-
-# #10
-# num_dict = {num_tuple:num_str}
-
-# #11
-# ans2 = len(num_list)
-
-# #12
-
-# #13
-# num_list.extend(['50','60']) 
-
-# #14
-# b = input()
-# is_under_50 =False
-# if b < 50:
-#     is_under_50 = True
-
-# #15
-
-# #16
-# print(dir(num_dict))
-
-
-# list = ['39', '32', '69']
-# for i, m in enumerate(list):
-#     print(i,m)
-    # print(m)
-
-# jaglj = ['aljg','ahgk','jaglj']
-# listB = [23,52,352,562,25]
-
-
-# for i,m in zip(jaglj, listB):
-#     print(i,m)
-
-# def print_hello(a,b):
-#     if a<b:
-#         print('ajglaj')
-#     else:
-#         print('jlgaj')
-
-
-# print_hello(23,53)
-
-# def janken():
-#     a = ['goo', 'tyoki', 'paa']
-#     b = input()
-#     print('あなたの出した手:{}、相手の出した手:{}'.format(b,a[0]))
-
-# janken()
-
-# y= 23
-# x=8462 if y<0 else 382
-# print(x)
-
-# lamda_a = lambda s, e, w: s + e + w
-# print(lamda_a(1,6,4))
-
-# listA = ['2','23',5,35,3,2,1]
-
-# list_new = [int(x) for x in listA]
-
-# print(list_new)
-
-# man = {
-#     'name': 'ichiro',
-#     'age':'21',
-#     'country':'japan'
-# }
-# print(man['name'])
-
-# man = [1,3,2,2,3,45,5,2]
-
-# map_man =  map(lambda y:y + ' ' + man[y], man)
-# for i in map_man:
-#     print(i)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# v = list.popitem()
+# print(list)
+# print(v)
