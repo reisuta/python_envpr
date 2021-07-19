f7da7306085688025a8aac74b2800ae2666829f1
amaki <in345vain@gmail.com>
2021-07-19 19:56:48 +0900

    add systemerror.html

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from logging import DEBUG
 import re
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, abort
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.template_filter('born_year')
+def calcurate_born_year(age):
+    now_timestamp = datetime.now()
+    return str(now_timestamp.year - int(age)) + '年'
+
+
+@app.template_filter('reverse_name')
+def reverse(name):
+    return name[-1::-1]
 
 @app.route('/')
 def index():
@@ -16,15 +26,40 @@ def home(user_name, age):
     }
     return render_template('home.html',user_info=login_user)
 
+class UserInfo:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
 @app.route('/userlist')
 def user_list():
+    # users = [
+    #     'Taro', 'Jiro', 'Saburo','Shiro'
+    # ]
     users = [
-        'Taro', 'Jiro', 'Saburo','Shiro'
+        UserInfo('Taro',21), UserInfo('Jiro',32), UserInfo('Hanako',31)
     ]
     is_login = True
     return render_template('userlist.html',users=users, is_login=is_login)
 
+@app.route('/user/<string:user_name>/<int:age>')
+def user(user_name, age):
+    if user_name in ['Taro', 'Jiro', 'Saburo']:
+        return redirect(url_for('home', user_name=user_name, age=age))
+    else:
+        abort(500, 'そのユーザはリダイレクトできません。')
 
+@app.errorhandler(500)
+def system_error(error):
+    error_description = error.description
+    return render_template('system_error.html', error_description=error_description), 500
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
