172210a24595f826d334b299ec3ae79212c04db5
amaki <in345vain@gmail.com>
2021-07-22 08:56:33 +0900

    upload add

--- form/app.py
+++ form/app.py
@@ -1,6 +1,7 @@
-from logging import debug
-from os import pipe
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import pykakasi
+import os
 
 app = Flask(__name__)
 
@@ -37,6 +38,22 @@ def home():
     )
 
     return render_template('home.html', user_info=user_info)
+
+@app.route('/upload', methods=["GET","POST"])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    elif request.method == 'POST':
+        file = request.files['file']
+        save_filename = secure_filename(file.filename)
+        file.save(os.path.join("./static/image", save_filename))
+        return redirect(url_for('uploaded_file', filename=save_filename))
+
+
+@app.route('/uploaded_file/<string:filename>')
+def uploaded_file(filename):
+    return render_template('uploaded_file.html',filename=filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
